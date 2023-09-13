873c14a745cad9913eeb133e1657d9969636c0db
Lim <limhsiuying90@gmail.com>
2023-09-13 16:59:33 +0800

    more rounds

--- route.rb
+++ route.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+enable :sessions
 
 get '/' do  #---'/' means direct to localhost or HOMEPAGE!
 	erb :index #this part direct you to index html page
@@ -6,15 +7,15 @@ end
 
 post '/game' do
 	@player_name = params[:name]
-	"Welcome to Sinatra! #{@player_name}"
+	@rounds = params[:rounds].to_i 
+	session[:name] = @player_name
+	session[:rounds] = @rounds
+	session[:results] = []
 	erb :game # this one redirect to any html page u want
 end
 
 post '/result' do  #---<form action = put link here at index file
-   #do something here
-   
-   @score = 0
-   @player_name = params[:name]
+      
    @player_choice = params[:choice]
    @computer_choice = ["Rock", "Paper", "Scissors"].sample
    
@@ -26,43 +27,34 @@ post '/result' do  #---<form action = put link here at index file
 	    	if @player_choice == "Scissors"
 	    		@outcome = "You Lose"
 	    	else
-	    		@outcome = "You WIN!"
-	    		@score += 1
+	    		@outcome = "You Win"	    		
 	    	end
     	when "Paper"
     		if @player_choice == "Rock"
 	    		@outcome = "You Lose"
 	    	else
-	    		@outcome = "You WIN!"
-	    		@score += 1
+	    		@outcome = "You Win"
 	    	end
     	when "Scissors"	
     		if @player_choice == "Paper"
 	    		@outcome = "You Lose"
 	    	else
-	    		@outcome = "You WIN!"
-	    		@score += 1
+	    		@outcome = "You Win"	
 	    	end
-
-      end 
-
+      end
    end
-
-   puts "#{@outcome}" #---will show in the server console, not html
-   puts "#{@score}"
-
+   history = { player: @player_choice, computer: @computer_choice, result: @outcome}
+   session[:results] << history
+   puts session[:results] 
+   if session[:results].length < session[:rounds]
+   	erb :game
+   else
+   	counter_winlose = session[:results].map { |hash| hash[:result]}
+   	session[:win_count] = counter_winlose.count("You Win")
+   	session[:lose_count] = counter_winlose.count("You Lose")
+   	session[:tie_count] = counter_winlose.count("TIE!")
+   	puts "#{counter_winlose}"
+   	erb :result
+   end
    
-   erb :result
 end
-
-# post '/result' do
-# 	@score = 1
-# 	if @outcome == "You WIN!"
-# 		@score += 1
-# 	else 
-# 		@score = 0
-# 	end
-# 	puts "#{@score}"
-#    erb :result
-# end
-
